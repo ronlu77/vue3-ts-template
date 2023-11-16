42cb5ab3f702903c4e0e997620b7f798930a8bbf
ronlu77 <ron000816@gmail.com>
2023-11-16 13:51:14 +0800

    feat: install global component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,21 @@
-import SvgIcon from '@/components/SvgIcon/index.vue'
-import LPagination from '@/components/Pagination/index.vue'
-import LTable from '@/components/Table/index.tsx'
-import LTableList from '@/components/TableList/index.tsx'
+import SvgIcon from './SvgIcon/index.vue'
+import LPagination from './Pagination/index.vue'
+import LTable from './Table/index.tsx'
+import LTableList from './TableList/index.tsx'
+import CountdownTimerDialog from './CountdownTimerDialog/index.vue'
+import ExpandCard from './ExpandCard/index.vue'
+import EllipsisText from './EllipsisText/index.vue'
+import Tooltip from './Tooltip/index.vue'
 
 const AllGlobalComponent: any = {
   SvgIcon,
   LPagination,
   LTable,
   LTableList,
+  CountdownTimerDialog,
+  ExpandCard,
+  EllipsisText,
+  Tooltip,
 }
 
 // 暴露全局组件注册插件对象
